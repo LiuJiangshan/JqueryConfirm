4897598e776ffd5f80f17b70e27084d43cb278fc
LiuJiangshan <1015905004@qq.com>
2017-03-14 10:17:22 +0800

    新增提示框

--- js/dialog.js
+++ js/dialog.js
@@ -53,3 +53,50 @@ function Confirm(msg, okDo, cancelDo)
         $dialog.appendTo($("body"));
     }
 }
+function Dialog(msg, okDo)
+{
+    var $dialog_msg = $("<div></div>");
+    {
+        $dialog_msg.attr("class", "dialog_msg");
+        $dialog_msg.html(msg);
+        $dialog_msg.click(function ()
+        {
+            event.stopPropagation();
+        });
+    }
+
+    var $dialog_ok_button = $("<input/>");
+    {
+        $dialog_ok_button.css("float", "right");
+        $dialog_ok_button.attr("type", "button");
+        $dialog_ok_button.attr("value", "确定");
+        $dialog_ok_button.attr("class", "dialog_button dialog_ok_button");
+        if (okDo != null && okDo != undefined)
+            $dialog_ok_button.click(okDo);
+    }
+    var $dialog_panel = $("<div></div>");
+    {
+        $dialog_panel.attr("class", "dialog_panel");
+    }
+    var $dialog = $("<div></div>");
+    {
+        $dialog.attr("class", "dialog");
+        $dialog.click(function ()
+        {
+            $dialog_panel.animate({width: "0%", height: "0%", top: "50%", left: "50%"}, 100, function ()
+            {
+                $dialog_panel.remove();
+            });
+            $dialog.animate({opacity: "0"}, 100, function ()
+            {
+                $dialog.remove();
+            });
+        });
+    }
+    {
+        $dialog_msg.appendTo($dialog_panel);
+        $dialog_ok_button.appendTo($dialog_panel);
+        $dialog_panel.appendTo($dialog);
+        $dialog.appendTo($("body"));
+    }
+}
